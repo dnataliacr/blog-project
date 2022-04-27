4c24bb550db32e3d63c51d726ea96f982736b38b
natalia <dnataliacr@gmail.com>
2022-04-27 16:16:36 -0300

    create fetchpost and fetchtags func

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -4,3 +4,24 @@
     </div>
   </div>
 </template>
+
+    fetchPost() {
+      let url;
+      this.value === ""
+        ? (url = `posts?_embed=1&per_page=${this.perPage}&page=${this.page}`)
+        : (url = "posts?tags=" + this.value.id);
+      fetch("https://www.kiterocket.com/wp-json/wp/v2/" + url)
+        .then((response) => response.json())
+        .then((data) => {
+          this.data = data;
+        });
+    },
+    fetchTags() {
+      fetch("https://www.kiterocket.com/wp-json/wp/v2/tags").then((response) =>
+        response.json().then((data) => {
+          data.forEach((item) => {
+            this.tags.push({ id: item.id, name: item.name });
+          });
+        })
+      );
+    },
